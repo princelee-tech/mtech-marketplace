38cb44b209dd71b7fa4dd9b92dc338ff6b57e1fa
princelee-tech <bamiseakinola56@gmail.com>
2026-03-03 21:14:39 +0100

    Add Header component for MTECH Marketplace

---
+++ app/components/Header.js
@@ -0,0 +1,15 @@
+export default function Header() {
+  return (
+    <header
+      style={{
+        background: "#0A192F",
+        color: "#fff",
+        padding: "20px",
+        textAlign: "center",
+        fontWeight: "bold"
+      }}
+    >
+      MTECH Marketplace 🔒
+    </header>
+  );
+}
